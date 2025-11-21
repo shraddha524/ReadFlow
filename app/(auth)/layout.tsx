7e2f1f8cf426fafbaa9a691d639c643926005653
Shraddha <shraddhamoily392@gmail.com>
2025-11-21 11:30:24 +0530

    Initial commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -14,7 +14,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
         <div className="auth-box">
           <div className="flex flex-row gap-3">
             <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
-            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+            <h1 className="text-2xl font-semibold text-white">ReadFlow</h1>
           </div>
 
           <div>{children}</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,9 @@ const bebasNeue = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "BookWise",
+  title: "ReadFlow",
   description:
-    "BookWise is a book borrowing university library management solution.",
+    "ReadFlow is a book borrowing university library management solution.",
 };
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
